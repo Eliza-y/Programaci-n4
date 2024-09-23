0ee53f70767e2badc475bab7ab91e991f18ee69d
Eliza-y <167116696+Eliza-y@users.noreply.github.com>
2024-09-23 15:29:37 -0300

    Add files via upload

---
+++ EjTPA Li Eliza/Ej1TP5ALiEliza.py
@@ -0,0 +1,5 @@
+##Ejercicio1
+def deci_Hola_amiga():
+    return print('¡Hola amiga!')
+
+deci_Hola_amiga()
---
+++ EjTPA Li Eliza/Ej2TP5ALiEliza.py
@@ -0,0 +1,8 @@
+##Ejercicio2
+nombre = input('tu numbre: ')
+
+def saludar (nombre):
+    print('hola ' + nombre)
+    return
+
+saludar (nombre)
